4afba594ad47f87e7884d5a92688a7b14a48cfdf
jainishgamit1025 <jainishgamit374@gmail.com>
2023-05-15 12:20:25 +0530

    This is a string builder To convert first letter of word convert in to uppercase

---
+++ src/com/jainish/Que7ConvertUppercase.java
@@ -0,0 +1,27 @@
+package com.jainish;
+//It will convert first letter of Word in to uppercase
+public class Que7ConvertUppercase {
+
+    public static String toUpperCase(String str){
+        StringBuilder sb = new StringBuilder(" ");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length()-1){ // i < str.length()-1 is used to avoid the index out of bound exception
+                sb.append(str.charAt(i));  // This is used to add the space in the string
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));  // This is used to convert the character in the uppercase
+            }
+            else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return  sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "hi, i am jainish";
+        System.out.println(toUpperCase(str));
+    }
+}
