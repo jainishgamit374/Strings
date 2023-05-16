ca1fbd0b59e73db28bfb54dced51fad308d197c2
jainishgamit1025 <jainishgamit374@gmail.com>
2023-05-16 14:38:33 +0530

    This is a string builder To compress the string using newString

---
+++ src/com/jainish/Que8StringCompression1.java
@@ -0,0 +1,26 @@
+package com.jainish;
+
+public class Que8StringCompression1 {
+
+    public static String compressString(String str){
+        String newStr = " ";
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+
+            while (i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if (count > 1) {
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbccddeeff";
+        System.out.println(compressString(str));
+    }
+}
