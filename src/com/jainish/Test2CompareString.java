b8478f863210280906213421b3817dfd8247a8c4
jainishgamit1025 <jainishgamit374@gmail.com>
2023-05-19 11:58:22 +0530

    Practice questions

---
+++ src/com/jainish/Test2CompareString.java
@@ -0,0 +1,10 @@
+package com.jainish;
+
+public class Test2CompareString {
+    public static void main(String[] args) {
+        String str = "Jainish";
+        String str1 = "Jainish";
+        String str2 = "Gandhi";
+        System.out.println(str.equals(str1)+ " " + str.equals(str2));
+    }
+}
---
+++ src/com/jainish/Test3Replace.java
@@ -0,0 +1,8 @@
+package com.jainish;
+
+public class Test3Replace {
+    public static void main(String[] args) {
+        String str = "Jainish".replace("i", "4");
+        System.out.println(str);
+    }
+}
